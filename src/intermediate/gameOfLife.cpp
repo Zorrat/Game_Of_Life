88684b9772a9899ce568b32e810c391bf16270f1
Zorrat <rohanthorat1230@gmail.com>
2022-07-28 18:30:36 +0530

    Finished intermediate version

--- src/intermediate/gameOfLife.cpp
+++ src/intermediate/gameOfLife.cpp
@@ -1,13 +1,85 @@
+#include <SDL2/SDL.h>
+#include <cstdlib>
 #include <iostream>
-#include "screen.h"
+#include <time.h>  
+#include <vector>
+#include <tuple>
+#include <algorithm>
 
-const int GAME_WIDTH = 64;
-const int GAME_HEIGHT = 64;
+using namespace std;
+
+// Setup SDL library
+class G {
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_bool done;
+    vector<SDL_FPoint> points;
+    vector<SDL_Color> colors;
+    SDL_Event event;
+    public:
+    G() 
+    {
+       SDL_Init(SDL_INIT_VIDEO); 
+       SDL_CreateWindowAndRenderer(1280, 720, SDL_WINDOW_ALLOW_HIGHDPI, &window, &renderer);
+       SDL_RenderSetScale(renderer,8,8);
+    }
+
+    void drawpixel(double xm, double ym, uint8_t r = 255, uint8_t g = 255, uint8_t b = 255, uint8_t a = 255) 
+    {
+        points.emplace_back(xm,ym);
+        colors.emplace_back(r,g,b,a);
+    }
+    void clearpixels()
+    {
+        points.clear();
+    }
+
+    void update() 
+    {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+        
+        for(long unsigned int i = 0; i < points.size(); i++)
+        {
+            SDL_SetRenderDrawColor(renderer, colors[i].r, colors[i].g, colors[i].b, colors[i].a);
+            SDL_RenderDrawPointF(renderer, points[i].x,points[i].y);
+        }
+
+
+        SDL_RenderPresent(renderer);
+    }
+    void input() {
+        while( SDL_PollEvent( &event ) )
+        {  
+            switch( event.type )
+            {
+                /* SDL_QUIT event (window close) */
+                case SDL_QUIT:
+                    SDL_Quit();
+                    exit(0);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+};
+
+
+const int GAME_WIDTH = 160;
+const int GAME_HEIGHT = 90;
+const float FRAME_RATE = 24;
+const float FRAME_INTERVAL = (1/FRAME_RATE) * 1000 ;
 
 
 int getRandomCellState(){
     int cellState;
-    cellState = rand() % 10==0 ? 1:0;
+    int rn = 0;;
+    rn = rand();
+    cellState = (rn % 10==0 || rn%3 ==0 ) ? 1:0;
     return cellState;
 }
 
@@ -40,16 +112,16 @@ int getAliveNeighbourCount(
 }
 
 // Here go the rules. Change as u wish
-bool isAlive(int liveNeighbourCount, bool currCellState){
+bool isAlive(int liveNeighbourCount, int currCellState){
     
     // If Current Cell is alive and less than 2 neighbours are alive then current cell dies.
-    if ( currCellState && liveNeighbourCount < 2) return false;
+    if ( currCellState==1 && liveNeighbourCount < 2) return false;
 
     // If Current cell is alive and has 2 or 3 alive neighbours then current cell stays alive.
-    if ( currCellState && ( liveNeighbourCount >1 && liveNeighbourCount < 4 ) )   return true;
+    if ( currCellState==1 && ( liveNeighbourCount >1 && liveNeighbourCount < 4 ) )   return true;
 
     // If Current Cell is dead and has exactly 3 alive neighbours then the current cell becomes alive
-    if ((!currCellState) && liveNeighbourCount==3 )    return true;
+    if ((currCellState ==0) && liveNeighbourCount==3 )    return true;
 
     // Anything else the current cell dies.
     return false;
@@ -59,7 +131,7 @@ int main(){
 
     std::array< std:: array<int,GAME_HEIGHT> , GAME_WIDTH > display {};
     std::array< std:: array<int,GAME_HEIGHT> , GAME_WIDTH> buffer {};
-    bool currCellState;
+    int currCellState;
     int liveNeighbourCount;
     G screen;
 
@@ -69,20 +141,12 @@ int main(){
         std::generate(row.begin(),row.end(),getRandomCellState );
     }
 
-    // for (auto& row: display){
-    //     for (int col:row){
-    //         std::cout << col;
-    //         std::cout << "|";
-    //     }
-    //     std:: cout << std::endl << std::string(GAME_WIDTH*2,'-') << std::endl;
-    // }
-
     while( true ){
         // Use the display 2D array as input. Apply rules and update to a buffer 2D array then swap buffer and display arrays
 
         for (int i=0; i< GAME_WIDTH;++i){
             for (int j=0 ; j< GAME_HEIGHT; ++j){
-                currCellState = (display[i][j] == 1)  ? true : false;
+                currCellState = display[i][j];
                 liveNeighbourCount = getAliveNeighbourCount(display,i,j);
                 // Update current Cell State in buffer
                 buffer[i][j] = isAlive(liveNeighbourCount,currCellState) ? 1 : 0 ; 
@@ -102,7 +166,7 @@ int main(){
 
         // Display to the screen.
         screen.update();
-        SDL_Delay(10);
+        SDL_Delay(FRAME_INTERVAL);
         screen.input();
         screen.clearpixels();
     }
